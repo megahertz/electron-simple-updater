5e264f9d68a437eae8ad8616269b616c7ffdd990
Alexey Prokhorov <megahertz@l96.ru>
2016-10-14 12:14:21 +0500

    Draft version, initialize an example

---
+++ example/README.md
@@ -0,0 +1,5 @@
+# Simple Updater Example
+
+This example uses 
+[electron-builder](https://github.com/electron-userland/electron-builder)
+for building installer and update package.
--- index.js
+++ index.js
@@ -10,7 +10,35 @@ const normalizeOptions = require('./lib/normalize-options');
 
 class SimpleUpdater extends events.EventEmitter {
   init(options) {
+    if (this.options) {
+      this.options.logger.warn(
+        'electron-simple updater has been initialized before'
+      );
+    }
+
     this.options = normalizeOptions(options);
+
+    const squirrelAction = win32.getSquirrelInstallerAction();
+    if (squirrelAction) {
+      const exitNow = this.options.onSquirrelWinInstaller(squirrelAction);
+      if (exitNow) {
+        process.exit();
+      }
+    }
+
+    if (this.options.checkUpdatesOnStart) {
+      this.checkForUpdates();
+    }
+
+    /**
+     * @event update-downloaded
+     * @param {Object} meta Update metadata
+     */
+    autoUpdater.on('update-downloaded', () => {
+      const version = this.meta.version;
+      this.options.logger.log(`New version ${version} has been downloaded`);
+      this.emit('update-downloaded', this.meta);
+    });
   }
 
   setFeedURL(url) {
@@ -26,11 +54,66 @@ class SimpleUpdater extends events.EventEmitter {
   }
 
   checkForUpdates() {
-    getUpdatesMeta(this.options.url)
+    const opt = this.options;
+
+    if (opts.disabled) {
+      opt.logger.warn(`Update is disabled`);
+    }
+
+    getUpdatesMeta(opt.url, opt.build, opt.channel, opt.version)
+      .then((updateMeta) => {
+        if (updateMeta) {
+          this.onFoundUpdate(updateMeta);
+        } else {
+          opt.logger.log(`Updates for ${this.build} are not available`);
+          this.emit('update-not-available');
+        }
+      })
+      .catch((e) => {
+        opt.logger.warn(e);
+        this.emit('error', e);
+      });
+  }
+
+  downloadUpdate() {
+    const feedUrl = autoUpdater.getFeedURL();
+    /**
+     * @event update-downloading
+     * @param {Object} meta Update metadata
+     */
+    this.emit('update-downloading', this.meta);
+    this.options.logger.log(`Downloading updates from ${feedUrl}`);
+
+    if (this.meta.platform === 'linux') {
+
+    } else {
+      autoUpdater.checkForUpdates();
+    }
   }
 
   quitAndInstall() {
     return autoUpdater.quitAndInstall();
+  }
+
+  /**
+   * Called when updates metadata has been downloaded
+   * @private
+   * @param meta
+   */
+  onFoundUpdate(meta) {
+    this.meta = meta;
+    const opt = this.options;
+
+    opt.logger.log(`Found version ${meta.name} at '${meta.updateUrl}'`);
+    autoUpdater.setFeedURL(meta.updateUrl);
+    /**
+     * @event update-available
+     * @param {Object} meta Update metadata
+     */
+    this.emit('update-available', meta);
+    if (opt.autoDownload) {
+      this.downloadUpdate();
+    }
   }
 }
 
